fc2dd65c0a18a5cb8d9bb21a315914ab3eeba0bb
Magnus Wideberg <magnus.wideberg@gmail.com>
2010-05-25 21:16:31 +0200

    Added function for splitting string into words

--- duelling/count_words.hs
+++ duelling/count_words.hs
@@ -2,23 +2,14 @@ module CountWords where
 
 import Articles
 
--- split_words :: String -> [String]
--- split_words x:xs
---     | x == ' ' = 
-
-
--- split_words' a x:xs
---     | xs == [] = 
---     | x == ' ' = split_words' (a ++ x) xs
---     | otherwise = split_words 
-
+split_words :: String -> [String]
+split_words "" = []
+split_words x = ws : split_words s
+    where (ws,s) = next_word x
+ 
 next_word :: String -> (String, String)
 next_word xs = next_word' ("", xs)
     where next_word' (a, x:xs)
+              | xs == "" = (a ++ [x],[])
               | x == ' ' = (a,xs)
               | otherwise = next_word' (a ++ [x], xs)
-                            
---next_word' :: (String, String) -> (String, String)
---n-ext_word' (a, x:xs)
---    | x == ' ' = (a,xs)
---    | otherwise = next_word' (a ++ [x], xs)
