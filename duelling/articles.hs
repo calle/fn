3a268a4aa775310c876c5ab5c74e50f997f64b47
Calle Wester <calle@lenson.net>
2010-05-25 20:22:12 +0200

    Finish Article data type

--- duelling/articles.hs
+++ duelling/articles.hs
@@ -1,10 +1,23 @@
 module Article where
 
 import Data.Time.LocalTime
+import Data.Time.Format
+import System.Locale
+import Data.Maybe
+
+type Title = String
+type Link = String
+type Content = String
 
 data Article = Article {
-  title :: String,
+  title :: Title,
   date  :: LocalTime,
-  link  :: String,
-  text  :: String
+  link  :: Link,
+  text  :: Content
 }
+instance Show Article where
+  show (Article title date link text) = "Article(" ++ title ++ " @ " ++ (show date) ++ ")"
+
+createArticle :: Title -> String -> Link -> Content -> Article
+createArticle title dateStr link content = Article title date link content
+  where date = fromJust (parseTime defaultTimeLocale "%Y-%m-%d %H:%M:%S" dateStr)
