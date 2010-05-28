c2a82cee1064f61c66f1b26a5c1abc74fe2061b6
Calle Wester <lennartsson@gmail.com>
2010-05-28 16:35:48 +0200

    Fix good methods in Main

--- duelling/Main.hs
+++ duelling/Main.hs
@@ -4,12 +4,27 @@ import Aftonbladet
 import Svd 
 import Articles
 import CountWords
+import List (sortBy)
 
-main = do
-  items <- aftonbladet 
-  -- items <- sofies_mode
-  -- items <- svd
-  putStrLn $ unlines $ take 5 (map showArticleWordCount items)
+items count = do
+  items <- fetch_items
+  putStrLn $ unlines $ take count (map showArticle (sortBy dateSort items))
+
+wordCount count = do
+  items <- fetch_items
+  putStrLn $ unlines $ take count (map showArticleWordCount (sortBy dateSort items))
+
+fetch_items :: IO [Article]
+fetch_items = fetch_all [aftonbladet, sofies_mode, svd]
+
+fetch_all :: [ IO [Article] ] -> IO [Article]
+fetch_all [] = return []
+fetch_all (x:xs) = do items <- x
+                      rest <- fetch_all xs
+                      return (items ++ rest)
+
+dateSort :: Article -> Article -> Ordering
+dateSort a b = compare (date b) (date a)
 
 showArticle :: Article -> String
 showArticle article = show article
