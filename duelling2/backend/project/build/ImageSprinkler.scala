6881b295ce6b92e2b3897763519db7ccebf0f47d
Calle Lennartsson <lennartsson@gmail.com>
2010-06-20 12:21:17 +0200

    Remove sbt project files and moved code to app/ directory

--- duelling2/backend/project/build/ImageSprinkler.scala
+++
@@ -1,9 +0,0 @@
-import sbt._
-
-class ImageSprinklerProject(info: ProjectInfo) extends DefaultProject(info) {
-
-  val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.22"
-
-  override def mainClass = Some("se.netlight.fn.imagesprinkler.Main")
-
-}
