b924ef41d11ec1278d6303b4a42fe1bc5b08c0dc
Calle Lennartsson <lennartsson@gmail.com>
2010-06-20 23:27:01 +0200

    Add simple sprinkler

---
+++ duelling2/sprinkler/app/controllers/Sprinkler.scala
@@ -0,0 +1,67 @@
+package controllers
+
+import play._
+import play.mvc._
+import play.libs.WS
+import java.io.File
+
+object Sprinkler extends Controller {
+
+  val root = "http://localhost:9000/sprinkle/ext"
+
+  def index = render()
+
+  def register {
+    println("Registering to " + root + "/register with url " + Router.getFullUrl("Sprinkler.send"))
+    
+    val result = WS.url(root + "/register")
+      .setParameter("name", "testing")
+      .setParameter("url", Router.getFullUrl("Sprinkler.send"))
+      .post();
+
+    renderText("ok: " + result + ", status: " + result.getStatus + "<br />" + result.getString)
+  }
+  
+  def send(id:String, title:String, description:String, file:File) {
+    println("Received sending of " + id)
+
+    // Make a slow handling with statuses
+    WS.url(root + "/status")
+      .setParameter("name", "testing")
+      .setParameter("id", id)
+      .setParameter("message", "starting")
+      .post()
+
+    Thread.sleep(2000)
+
+    WS.url(root + "/status")
+      .setParameter("name", "testing")
+      .setParameter("id", id)
+      .setParameter("message", "after 2s")
+      .post()
+
+
+    Thread.sleep(3000)
+
+    WS.url(root + "/status")
+      .setParameter("name", "testing")
+      .setParameter("id", id)
+      .setParameter("message", "after 5s")
+      .post()
+
+    Thread.sleep(1000)
+
+    WS.url(root + "/done")
+      .setParameter("name", "testing")
+      .setParameter("id", id)
+      .post()
+  }
+
+  def unregister {
+    WS.url(root + "/unregister")
+      .setParameter("name", "testing")
+      .post();
+    renderText("ok")
+  }
+
+}
