55f29c9d49e0001fadf56cd7ff9e33826dc2d531
Calle Wester <lennartsson@gmail.com>
2010-06-23 18:57:29 +0200

    Update backend web

--- duelling2/backend/app/controllers/Sprinkler.scala
+++ duelling2/backend/app/controllers/Sprinkler.scala
@@ -16,23 +16,33 @@ object Sprinkler extends Controller {
   def index = render()
 
   def test() {
-    val id = UUID.randomUUID.toString
-    val photo = Photo(id, "title", "description", new ImageFile(new File("public/images/fn.jpg")))
+    val uuid = UUID.randomUUID.toString
+    val photo = Photo(uuid, "title", "description", new ImageFile(new File("public/images/fn.jpg")))
     println("sending photo: " + photo)
     backend.map( _ ! Send(photo) )
-    renderText(id)
+    render(uuid)
   }
 
   def send(title:String, description:String, file:File) {
-    val id = UUID.randomUUID.toString
-    val photo = Photo(id, title, description, new ImageFile(file))
+    val uuid = UUID.randomUUID.toString
+    val photo = Photo(uuid, title, description, new ImageFile(file))
     println("sending photo: " + photo)
     backend.map( _ ! Send(photo) )
-    renderText(id)
+    render(uuid)
   }
 
   def status(uuid: String) {
-    renderText("status for " + uuid)
+    backend match {
+    	case Some(b) => b !? Backend.GetStatus(uuid) match {
+    		case Backend.GetStatusResponse(statuses) => {
+    			renderText(statuses.map { 
+    				case (key, statuses) => "\"" + key + "\": " + statuses.map { "\"" + _ + "\"" }.mkString("[", ",", "]")
+    			}.mkString("{", ",", "}"))
+    		}
+    		case _ => renderText("{ error:\"Unknown uuid: " + uuid + "\"}");
+    	}
+    	case _ => renderText("{ error:\"Not started correctly\"}");
+    }
   }
 
   def shutdown() {
