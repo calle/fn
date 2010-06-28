f1d425e2194a699900665fc62bcf647d52e73e62
Calle Wester <lennartsson@gmail.com>
2010-06-28 18:32:18 +0200

    Add simple sprinkler to show updates in browser

---
+++ duelling2/backend/app/imagesprinkler/sprinkler/SimpleSprinkler.scala
@@ -0,0 +1,48 @@
+package imagesprinkler.sprinkler
+
+
+import scala.actors.Actor
+import scala.actors.Actor._
+import scala.actors.OutputChannel
+import scala.collection.mutable.{ Buffer => MBuffer }
+
+import imagesprinkler.Photo
+
+object SimpleSprinkler {
+	case class GetPhoto()
+	case class GetNextPhoto()
+	case class GetPhotoResult(photo:Option[Photo])
+}
+
+class SimpleSprinkler() extends Sprinkler {
+
+  var name = "simple sprinkler"
+
+  private var photo : Option[Photo] = None
+  private val awaitingPhoto : MBuffer[OutputChannel[Any]] = MBuffer()
+  
+  def act() {
+    while (true) {
+      receive {
+        case Send(photo) => {
+          reply(Started(this, PhotoInstance(photo, this.name)));
+          this.photo = Some(photo)
+          awaitingPhoto.foreach { actor => actor ! SimpleSprinkler.GetPhotoResult(Some(photo)) }
+          awaitingPhoto.clear
+          reply(Complete(this, PhotoInstance(photo, this.name)));
+        }
+        case Shutdown => {
+          this.photo = None
+          return;
+        }
+        case SimpleSprinkler.GetPhoto => {
+          reply(SimpleSprinkler.GetPhotoResult(photo))
+        }
+        case SimpleSprinkler.GetNextPhoto => {
+          awaitingPhoto.append(sender)
+        }
+      }
+    }
+  }
+
+}
