d45b71caaa93739cb189ad7d4223c2f7f7107bf4
Calle Lennartsson <lennartsson@gmail.com>
2010-07-01 21:43:28 +0200

    Whitespace in SimpleSprinklerController

--- duelling2/backend/app/controllers/SimpleSprinklerController.scala
+++ duelling2/backend/app/controllers/SimpleSprinklerController.scala
@@ -9,54 +9,54 @@ import imagesprinkler.Photo
 import imagesprinkler.sprinkler.SimpleSprinkler
 
 object SimpleSprinklerController extends Controller {
-  
+
   var sprinkler:Option[SimpleSprinkler] = None
 
   def index = render()
 
   def photo = {
-  sprinkler match {
-    case Some(sprinkler) => {
-      sprinkler !? SimpleSprinkler.GetPhoto match {
-        case SimpleSprinkler.GetPhotoResult(Some(photo)) => renderPhoto(photo)
-        case _ => renderText("{}")
+    sprinkler match {
+      case Some(sprinkler) => {
+        sprinkler !? SimpleSprinkler.GetPhoto match {
+          case SimpleSprinkler.GetPhotoResult(Some(photo)) => renderPhoto(photo)
+          case _ => renderText("{}")
+        }
       }
+      case _ => renderText("{}")
     }
-    case _ => renderText("{}")
-  }
   }
 
   def nextPhoto = {
     if(request.isNew) {
         val task = new GetNextPhotoJob().now()
         request.args.put("task", task)
-        waitFor(task)
+        waitFor(task);
     }
     request.args.get("task") match {
       case future:Future[Option[Photo]] => future.get() match {
-      case Some(photo) => renderPhoto(photo)
-      case _ => renderText("{}")
+        case Some(photo) => renderPhoto(photo);
+        case _ => renderText("{}");
       }
     }
   }
 
-  private def renderPhoto(photo:Photo) = 
-  renderJSON(JsonPhoto(photo.id, photo.image.asBase64))
+  private def renderPhoto(photo:Photo) =
+    renderJSON(JsonPhoto(photo.id, photo.image.asBase64));
   
-  private case class JsonPhoto(id:String, data:String) 
+  private case class JsonPhoto(id:String, data:String);
 
   private class GetNextPhotoJob extends Job[Option[Photo]] {
-  override def doJobWithResult() : Option[Photo] = {
-    SimpleSprinklerController.sprinkler match {
-      case Some(sprinkler) => {
-       sprinkler !? SimpleSprinkler.GetNextPhoto match {
-          case SimpleSprinkler.GetPhotoResult(Some(photo)) => Some(photo)
-          case _ => None
-        }
-     }
-     case _ => None
+    override def doJobWithResult() : Option[Photo] = {
+      SimpleSprinklerController.sprinkler match {
+        case Some(sprinkler) => {
+          sprinkler !? SimpleSprinkler.GetNextPhoto match {
+            case SimpleSprinkler.GetPhotoResult(Some(photo)) => Some(photo)
+            case _ => None
+          }
+       }
+       case _ => None
+      }
     }
-  }
   }
 
 }
