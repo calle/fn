8032f397a981509f34e191f6601ba0269437dcbe
Calle Wester <calle@lenson.net>
2010-09-14 19:58:42 +0200

    Add express webapp

---
+++ duelling3/front/app.js
@@ -0,0 +1,36 @@
+var express = require('express'),
+    connect = require('connect');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.use(connect.bodyDecoder());
+    app.use(connect.methodOverride());
+    app.use(connect.compiler({ src: __dirname + '/public', enable: ['less'] }));
+    app.use(app.router);
+    app.use(connect.staticProvider(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+    app.use(connect.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+   app.use(connect.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res){
+    res.render('index.jade', {
+        locals: {
+            title: 'Battlefield'
+        }
+    });
+});
+
+// Only listen on $ node app.js
+if (!module.parent) app.listen(3000);
