c36963f229dccc44f8a0d478904d4dfad37eb098
Calle Wester <lennartsson@gmail.com>
2010-09-15 16:37:52 +0200

    Adding simple test-client

---
+++ duelling3/front/lib/mockClient.js
@@ -0,0 +1,33 @@
+var Battlefield = require('./battlefield');
+
+
+
+var battlefield = new Battlefield('localhost', 3001);
+
+var id = '344'
+
+battlefield.login(id, 'calle', {
+  login: function(err, resp) {
+    console.log('successfull login');
+    
+    battlefield.move(id, 'east');
+    battlefield.move(id, 'east');
+    battlefield.move(id, 'east');
+    battlefield.move(id, 'east');
+    battlefield.move(id, 'east');
+
+    battlefield.shoot(id, 3, 6);
+
+    battlefield.taunt(id, 'olle', 'your mama');
+
+    battlefield.logout();
+  },
+  update: function(message) {
+    console.log('got update: ' + message);
+  },
+  end: function() {
+    console.log('client terminated');
+  }
+});
+
+
