f858b46169c52aef466fbe82b9340d0293f7049d
Magnus Wideberg <magnus.wideberg@gmail.com>
2011-12-06 19:12:21 +0100

    adding moveCards

--- dojo2/patiens.scala
+++ dojo2/patiens.scala
@@ -1,19 +1,28 @@
+def moveCards(l:List[Int]) : List[Int] = {
+	var list = l :: 12;
+	return list;
+}
+
 def readInput():List[Int] = {
 	var l:List[Int] = Nil;
-	while (true){
+	while (true) {
 		try{
 			var c = readInt;
-			c :: l;
+			l = c :: l;
+			//println(c);
 		}
 		catch{
 			case e: Exception =>
 				return l;
 		}
 	}
+	return Nil;
 }
 
 var a = 17;
 var b = 19;
 println(a+b);
 var l = readInput();
+println(l);
+l = moveCards(l);
 println(l);
