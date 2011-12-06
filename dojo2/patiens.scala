e549e16eae669c085373547757895ec248fbe103
Magnus Wideberg <magnus.wideberg@gmail.com>
2011-12-06 20:10:02 +0100

    Test! Test! TESTESTERRES!! \o/

--- dojo2/patiens.scala
+++ dojo2/patiens.scala
@@ -1,10 +1,10 @@
 def compressList(l:List[Int]) : List[Int] = {
-	for(i <- l; if i != 0) yield i;
+	for(i <- l; if i > 0) yield i;
 }
 
 def moveCards(l:List[Int]) : List[Int] = {
 	val j = for(i <- l) yield i-1
-	(compressList(l.size :: j)).sort(_>_)
+	(compressList(l.size :: j)).sortWith(_>_)
 }
 
 def readInput():List[Int] = {
@@ -21,6 +21,14 @@ def readInput():List[Int] = {
 	l;
 }
 
+def bulgaric_patiens(){
+	//var l = readInput();
+	
+	//while(changed){
+		moveCards
+	}
+}
+
 //var l = readInput();
 
 
@@ -31,6 +39,9 @@ def readInput():List[Int] = {
 //l = moveCards(l);
 //println(l);
 
-println(moveCards(List(2,1)))
+println(moveCards(List(0, -1, 1, 2,3)))
 println(1, moveCards(List(2,1)) == List(2,1))
 println(2, moveCards(List(1,2,3)) == List(3,2,1))
+println(3, moveCards(List(0,-1,1,2,3)) == List(5,2,1))
+
+
