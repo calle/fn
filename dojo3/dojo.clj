d880fe1ecd5fc5c15026de35770f797f8a3b1182
Mårten Sjöstrand <marten.sjostrand@gmail.com>
2012-06-14 19:16:26 +0200

    Sedriks try 1

--- dojo3/dojo.clj
+++ dojo3/dojo.clj
@@ -1,10 +1,12 @@
+(require '[clj-http.client :as client])
+
+(def products (list 123 234 212 225))
+
 (def baseUrl "http://www.systembolaget.se/Sok-dryck/Dryck/?varuNr=")
 
 (defn buildUrl
       ([articleNr] (str baseUrl articleNr)))
 
-(println (buildUrl 17))
-; "http://www.systembolaget.se/Sok-dryck/Dryck/?varuNr=11621"
-
+(println (map buildUrl products))
 
 (do (map println *command-line-args*))
