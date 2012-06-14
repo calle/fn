e13258354853a818ba645b0c7e9fc84821260df1
Mårten Sjöstrand <marten.sjostrand@gmail.com>
2012-06-14 19:08:29 +0200

    Mårtens försök 1

--- dojo3/dojo.clj
+++ dojo3/dojo.clj
@@ -1,8 +1,10 @@
-(println "hej!")
+(def baseUrl "http://www.systembolaget.se/Sok-dryck/Dryck/?varuNr=")
 
 (defn buildUrl
-      ([x] "ret")
-     )
+      ([articleNr] (str baseUrl articleNr)))
 
 (println (buildUrl 17))
 ; "http://www.systembolaget.se/Sok-dryck/Dryck/?varuNr=11621"
+
+
+(do (map println *command-line-args*))
